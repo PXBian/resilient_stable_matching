da8bc03f435a374e099da026aab6c7e27648c6b0
Pengxin Bian <1053189561@qq.com>
2025-12-12 21:25:51 +0000

    Update datasets and script

--- total_performance.sh
+++ total_performance.sh
@@ -1,70 +1,43 @@
 #!/bin/bash
 
 mkdir peak_ram
-gunzip *.gz
+gunzip data/*.gz
 
-g++ -O3 -std=c++17 total_test.cpp \
-    -Ilemon-1.3.1/build/liblemon/include \
-    -Llemon-1.3.1/build/liblemon/lib \
-    -o total_test
+make total
 
 
-# (/usr/bin/time -v ./total_test 10000 1000 200 ) &> peak_ram/total_test_10000_1000_200.txt
-# (/usr/bin/time -v ./total_test 20000 1000 200 ) &> peak_ram/total_test_20000_1000_200.txt
-# (/usr/bin/time -v ./total_test 30000 1000 200 ) &> peak_ram/total_test_30000_1000_200.txt
-# (/usr/bin/time -v ./total_test 40000 1000 200 ) &> peak_ram/total_test_40000_1000_200.txt
-# (/usr/bin/time -v ./total_test 50000 1000 200 ) &> peak_ram/total_test_50000_1000_200.txt
+(/usr/bin/time -v ./total_test data/ADM_1000_instance.txt 1000 50 ) &> peak_ram/total_test_adm1000_1000_50.txt
+(/usr/bin/time -v ./total_test data/ADM_2000_instance.txt 2000 50 ) &> peak_ram/total_test_adm2000_2000_50.txt
+(/usr/bin/time -v ./total_test data/ADM_3000_instance.txt 3000 50 ) &> peak_ram/total_test_adm3000_3000_50.txt
+(/usr/bin/time -v ./total_test data/ADM_4000_instance.txt 4000 50 ) &> peak_ram/total_test_adm4000_4000_50.txt
+(/usr/bin/time -v ./total_test data/ADM_5000_instance.txt 5000 50 ) &> peak_ram/total_test_adm5000_5000_50.txt
 
-# (/usr/bin/time -v ./total_test 10000 1000 400 ) &> peak_ram/total_test_10000_1000_400.txt
-# (/usr/bin/time -v ./total_test 20000 1000 400 ) &> peak_ram/total_test_20000_1000_400.txt
-# (/usr/bin/time -v ./total_test 30000 1000 400 ) &> peak_ram/total_test_30000_1000_400.txt
-# (/usr/bin/time -v ./total_test 40000 1000 400 ) &> peak_ram/total_test_40000_1000_400.txt
-# (/usr/bin/time -v ./total_test 50000 1000 400 ) &> peak_ram/total_test_50000_1000_400.txt
+(/usr/bin/time -v ./total_test data/ADM_1000_instance.txt 1000 100) &> peak_ram/total_test_adm1000_1000_100.txt
+(/usr/bin/time -v ./total_test data/ADM_2000_instance.txt 2000 100) &> peak_ram/total_test_adm2000_2000_100.txt
+(/usr/bin/time -v ./total_test data/ADM_3000_instance.txt 3000 100) &> peak_ram/total_test_adm3000_3000_100.txt
+(/usr/bin/time -v ./total_test data/ADM_4000_instance.txt 4000 100) &> peak_ram/total_test_adm4000_4000_100.txt
+(/usr/bin/time -v ./total_test data/ADM_5000_instance.txt 5000 100) &> peak_ram/total_test_adm5000_5000_100.txt
 
-# (/usr/bin/time -v ./total_test 10000 1000 600 ) &> peak_ram/total_test_10000_1000_600.txt
-# (/usr/bin/time -v ./total_test 20000 1000 600 ) &> peak_ram/total_test_20000_1000_600.txt
-# (/usr/bin/time -v ./total_test 30000 1000 600 ) &> peak_ram/total_test_30000_1000_600.txt
-# (/usr/bin/time -v ./total_test 40000 1000 600 ) &> peak_ram/total_test_40000_1000_600.txt
-# (/usr/bin/time -v ./total_test 50000 1000 600 ) &> peak_ram/total_test_50000_1000_600.txt
+(/usr/bin/time -v ./total_test data/ADM_1000_instance.txt 1000 150) &> peak_ram/total_test_adm1000_1000_150.txt
+(/usr/bin/time -v ./total_test data/ADM_2000_instance.txt 2000 150) &> peak_ram/total_test_adm2000_2000_150.txt
+(/usr/bin/time -v ./total_test data/ADM_3000_instance.txt 3000 150) &> peak_ram/total_test_adm3000_3000_150.txt
+(/usr/bin/time -v ./total_test data/ADM_4000_instance.txt 4000 150) &> peak_ram/total_test_adm4000_4000_150.txt
+(/usr/bin/time -v ./total_test data/ADM_5000_instance.txt 5000 150) &> peak_ram/total_test_adm5000_5000_150.txt
 
-# (/usr/bin/time -v ./total_test 10000 1000 800 ) &> peak_ram/total_test_10000_1000_800.txt
-# (/usr/bin/time -v ./total_test 20000 1000 800 ) &> peak_ram/total_test_20000_1000_800.txt
-# (/usr/bin/time -v ./total_test 30000 1000 800 ) &> peak_ram/total_test_30000_1000_800.txt
-# (/usr/bin/time -v ./total_test 40000 1000 800 ) &> peak_ram/total_test_40000_1000_800.txt
-# (/usr/bin/time -v ./total_test 50000 1000 800 ) &> peak_ram/total_test_50000_1000_800.txt
+(/usr/bin/time -v ./total_test data/ADM_1000_instance.txt 1000 200) &> peak_ram/total_test_adm1000_1000_200.txt
+(/usr/bin/time -v ./total_test data/ADM_2000_instance.txt 2000 200) &> peak_ram/total_test_adm2000_2000_200.txt
+(/usr/bin/time -v ./total_test data/ADM_3000_instance.txt 3000 200) &> peak_ram/total_test_adm3000_3000_200.txt
+(/usr/bin/time -v ./total_test data/ADM_4000_instance.txt 4000 200) &> peak_ram/total_test_adm4000_4000_200.txt
+(/usr/bin/time -v ./total_test data/ADM_5000_instance.txt 5000 200) &> peak_ram/total_test_adm5000_5000_200.txt
 
-# (/usr/bin/time -v ./total_test 10000 1000 1000 ) &> peak_ram/total_test_10000_1000_1000.txt
-# (/usr/bin/time -v ./total_test 20000 1000 1000 ) &> peak_ram/total_test_20000_1000_1000.txt
-# (/usr/bin/time -v ./total_test 30000 1000 1000 ) &> peak_ram/total_test_30000_1000_1000.txt
-# (/usr/bin/time -v ./total_test 40000 1000 1000 ) &> peak_ram/total_test_40000_1000_1000.txt
-# (/usr/bin/time -v ./total_test 50000 1000 1000 ) &> peak_ram/total_test_50000_1000_1000.txt
+(/usr/bin/time -v ./total_test data/ADM_1000_instance.txt 1000 250) &> peak_ram/total_test_adm1000_1000_250.txt
+(/usr/bin/time -v ./total_test data/ADM_2000_instance.txt 2000 250) &> peak_ram/total_test_adm2000_2000_250.txt
+(/usr/bin/time -v ./total_test data/ADM_3000_instance.txt 3000 250) &> peak_ram/total_test_adm3000_3000_250.txt
+(/usr/bin/time -v ./total_test data/ADM_4000_instance.txt 4000 250) &> peak_ram/total_test_adm4000_4000_250.txt
+(/usr/bin/time -v ./total_test data/ADM_5000_instance.txt 5000 250) &> peak_ram/total_test_adm5000_5000_250.txt
 
-(/usr/bin/time -v ./total_test 100000 1000 200 ) &> peak_ram/total_test_100000_1000_200.txt
-(/usr/bin/time -v ./total_test 100000 1000 400 ) &> peak_ram/total_test_100000_1000_400.txt
-(/usr/bin/time -v ./total_test 100000 1000 600 ) &> peak_ram/total_test_100000_1000_600.txt
-(/usr/bin/time -v ./total_test 100000 1000 800 ) &> peak_ram/total_test_100000_1000_800.txt
-(/usr/bin/time -v ./total_test 100000 1000 1000) &> peak_ram/total_test_100000_1000_1000.txt
-
-(/usr/bin/time -v ./total_test 200000 1000 200 ) &> peak_ram/total_test_200000_1000_200.txt
-(/usr/bin/time -v ./total_test 200000 1000 400 ) &> peak_ram/total_test_200000_1000_400.txt
-(/usr/bin/time -v ./total_test 200000 1000 600 ) &> peak_ram/total_test_200000_1000_600.txt
-(/usr/bin/time -v ./total_test 200000 1000 800 ) &> peak_ram/total_test_200000_1000_800.txt
-(/usr/bin/time -v ./total_test 200000 1000 1000) &> peak_ram/total_test_200000_1000_1000.txt
-
-(/usr/bin/time -v ./total_test 300000 1000 200 ) &> peak_ram/total_test_300000_1000_200.txt
-(/usr/bin/time -v ./total_test 300000 1000 400 ) &> peak_ram/total_test_300000_1000_400.txt
-(/usr/bin/time -v ./total_test 300000 1000 600 ) &> peak_ram/total_test_300000_1000_600.txt
-(/usr/bin/time -v ./total_test 300000 1000 800 ) &> peak_ram/total_test_300000_1000_800.txt
-(/usr/bin/time -v ./total_test 300000 1000 1000) &> peak_ram/total_test_300000_1000_1000.txt
-
-(/usr/bin/time -v ./total_test 400000 1000 200 ) &> peak_ram/total_test_400000_1000_200.txt
-(/usr/bin/time -v ./total_test 400000 1000 400 ) &> peak_ram/total_test_400000_1000_400.txt
-(/usr/bin/time -v ./total_test 400000 1000 600 ) &> peak_ram/total_test_400000_1000_600.txt
-(/usr/bin/time -v ./total_test 400000 1000 800 ) &> peak_ram/total_test_400000_1000_800.txt
-(/usr/bin/time -v ./total_test 400000 1000 1000) &> peak_ram/total_test_400000_1000_1000.txt
-
-(/usr/bin/time -v ./total_test 500000 1000 200 ) &> peak_ram/total_test_500000_1000_200.txt
-(/usr/bin/time -v ./total_test 500000 1000 400 ) &> peak_ram/total_test_500000_1000_400.txt
-(/usr/bin/time -v ./total_test 500000 1000 600 ) &> peak_ram/total_test_500000_1000_600.txt
-(/usr/bin/time -v ./total_test 500000 1000 800 ) &> peak_ram/total_test_500000_1000_800.txt
-(/usr/bin/time -v ./total_test 500000 1000 1000) &> peak_ram/total_test_500000_1000_1000.txt
+(/usr/bin/time -v ./total_test data/ADM_1000_instance.txt 1000 300) &> peak_ram/total_test_adm1000_1000_300.txt
+(/usr/bin/time -v ./total_test data/ADM_2000_instance.txt 2000 300) &> peak_ram/total_test_adm2000_2000_300.txt
+(/usr/bin/time -v ./total_test data/ADM_3000_instance.txt 3000 300) &> peak_ram/total_test_adm3000_3000_300.txt
+(/usr/bin/time -v ./total_test data/ADM_4000_instance.txt 4000 300) &> peak_ram/total_test_adm4000_4000_300.txt
+(/usr/bin/time -v ./total_test data/ADM_5000_instance.txt 5000 300) &> peak_ram/total_test_adm5000_5000_300.txt
