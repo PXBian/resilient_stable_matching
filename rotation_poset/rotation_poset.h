1e218b31a0c1dffb0836071b4c6877914943bfce
AlbeLepi <113667713+AlbeLepi@users.noreply.github.com>
2026-05-08 12:50:36 +0200

    Competitor implementation

--- rotation_poset/rotation_poset.h
+++ rotation_poset/rotation_poset.h
@@ -38,8 +38,22 @@ typedef struct {
     size_t n;
 } PositionMapMatrix;
 
+typedef struct {
+    int from; 
+    int to;
+    int cost;
+} Arc;
+
+typedef struct {
+    size_t n_vertices; // number of vertices
+    Arc* arcs_list; // pointer
+    size_t n_arcs; // length
+} StablePairsGraph;
+
 extern RotationDigraph get_rotation_digraph(RankingListMatrix men, PositionMapMatrix women, int complete_data);
 extern void free_rotation_digraph(RotationDigraph digraph);
+extern StablePairsGraph get_stable_pairs_graph(RotationDigraph digraph);
+extern void free_stable_pairs_graph(StablePairsGraph graph);
 
 void invert_matrix(int* matrix, size_t n) {
     int* temp = (int*)malloc(n * sizeof(int));
