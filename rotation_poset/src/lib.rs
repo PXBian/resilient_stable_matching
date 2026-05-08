73403ff940a3d17a98d10aa9aa19264c4fd8c7a1
AlbeLepi <113667713+AlbeLepi@users.noreply.github.com>
2026-05-08 12:49:24 +0200

    Competitor implementation

--- rotation_poset/src/lib.rs
+++ rotation_poset/src/lib.rs
@@ -5,11 +5,13 @@ pub mod rotation_digraph;
 pub mod matching;
 pub mod rotations;
 pub mod algorithms;
+pub mod stable_pairs_graph;
 
 pub use ranking_position::{RankingListMatrix, PositionMapMatrix};
 pub use profile::Profile;
-pub use rotation_digraph::{RotationDigraph, Dependency};
+pub use rotation_digraph::RotationDigraph;
 pub use rotation_set::RotationSet;
+pub use stable_pairs_graph::StablePairsGraph;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn get_rotation_digraph(men: RankingListMatrix, women: PositionMapMatrix, complete_data: u8) -> RotationDigraph {
@@ -40,6 +42,22 @@ pub extern "C" fn free_rotation_digraph(arr: RotationDigraph) {
         unsafe {
             // Reconstruct the Vec safely
             let _ = Vec::from_raw_parts(arr.pairs_list, arr.n_pairs, arr.n_pairs);
+            // when it goes out of scope, the Vec is dropped
+        }
+    }
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn get_stable_pairs_graph(rd: RotationDigraph) -> StablePairsGraph {
+    StablePairsGraph::from_rotation_digraph(&rd)
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn free_stable_pairs_graph(arr: StablePairsGraph) {
+    if !arr.arcs_list.is_null() {
+        unsafe {
+            // Reconstruct the Vec safely
+            let _ = Vec::from_raw_parts(arr.arcs_list, arr.n_arcs, arr.n_arcs);
             // when it goes out of scope, the Vec is dropped
         }
     }
